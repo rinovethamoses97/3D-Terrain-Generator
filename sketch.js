1b6ba073d3203f299d63a320a34e23dac7efe6f3
rino <rinovethamoses97@gmail.com>
2019-07-17 13:10:54 +0530

    bug fixing and full screen mode added

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let rows;
 let cols;
 let z_values=[];
 function setup(){
-    createCanvas(500,500,WEBGL);
+    createCanvas(windowWidth,windowHeight,WEBGL);
     rows=1000/size;
-    cols=1000/size;
+    cols=2200/size;
     let x_off=0;
     let y_off=0;
     for(let i=0;i<rows+1;i++){
@@ -24,16 +24,16 @@ function draw(){
     background(0);
     angleMode(DEGREES);
     rotateX(50);
-    translate(-width/2-200,-height/2-500);
+    translate(-width/2-400,-height/2-450);
     stroke(255);
     noFill();
     for(let i=0;i<rows;i++){
         beginShape(TRIANGLE_STRIP);
         for(let j=0;j<cols;j++){
-            vertex(j*size,i*size,z_values[j][i]);
-            vertex(j*size,(i+1)*size,z_values[j][i+1]);    
-            vertex((j+1)*size,i*size,z_values[j+1][i]);
-            vertex((j+1)*size,(i+1)*size,z_values[j+1][i+1]);
+            vertex(j*size,i*size,z_values[i][j]);
+            vertex(j*size,(i+1)*size,z_values[i+1][j]);    
+            vertex((j+1)*size,i*size,z_values[i][j+1]);
+            vertex((j+1)*size,(i+1)*size,z_values[i+1][j+1]);
         }
         endShape();
     }
